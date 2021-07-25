c8587c42b7596b52aa967f1bf9124edbd002b052
Andrei Dzebysh <presnia@yahoo.com>
2021-07-25 15:31:33 +0300

    All functionality is implemented

--- src/components/AccordionItem/AccordionItem.js
+++ src/components/AccordionItem/AccordionItem.js
@@ -1,13 +1,22 @@
+import {useRef} from "react";
 import './AccordionItem.css';
 
-const AccordionItem = ({faq: {question, answer}, active, onToggle}) => {
+const AccordionItem = ({ faq: {question, answer}, active, onToggle }) => {
+    const contentElement = useRef();
+
     return (
         <li className={`accordion_item ${active ? 'active' : ''}`}>
             <button className="button" onClick={onToggle}>
                 {question}
                 <span className="control">{active ? '-' : '+'}</span>
             </button>
-            <div className={`answer_wrapper ${active ? 'open' : ''}`}>
+            <div
+                ref={contentElement}
+                className={`answer_wrapper ${active ? 'open' : ''}`}
+                style={
+                    active ? { height: contentElement.current.scrollHeight } : { height: '0px'}
+                }
+            >
                 <div className="answer">{answer}</div>
             </div>
         </li>
